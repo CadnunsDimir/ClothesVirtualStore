aebe5e78cf5515c96c781d0514f33bec4ab0eee2
Tiago Silva do Nascimento <cadnuns.dimir@gmail.com>
2024-02-26 19:06:36 -0300

    feat(consumers.cartcheckout): initial integration

--- consumers/CartCheckout/Program.cs
+++ consumers/CartCheckout/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+string queueName = "ClothesVirtualStore.Cart.Checkout";
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "admin",
+    Password = "123456"
+};
+
+using var connection = factory.CreateConnection();
+using (var channel = connection.CreateModel()){
+    channel.QueueDeclare(
+        queue: queueName,
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null
+    );
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) => {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine($"[x] Received {message}");
+
+        Console.WriteLine($"[x] ... Begining Processing  ...");
+
+        
+    };
+
+    channel.BasicConsume(
+        queue: queueName,
+        autoAck: true,
+        consumer
+    );
+
+    Console.WriteLine("Press [enter] to exit.");
+    Console.ReadKey();
+}
+
---
+++ teste_api_consumer.md
@@ -0,0 +1,7 @@
+## rodar os comandos abaixo em terminais diferentes:
+
+```
+dotnet run --project consumers/CartCheckout/
+dotnet run --project api/ClothesVirtualStore.Api.Cart/
+dotnet run --project api/ClothesVirtualStore.Api.Products/
+```
