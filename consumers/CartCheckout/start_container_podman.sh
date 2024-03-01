da27b1544924d39c1a7c2ab1202a089209bf8376
Tiago Silva do Nascimento <cadnuns.dimir@gmail.com>
2024-03-01 12:18:59 -0300

    chore(consumer.cart): improve podman image buid

---
+++ consumers/CartCheckout/start_container_podman.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+MICROSERVICE_NAME=cart_consumer_microservice
+COMMON_LIB=ClothesVirtualStore.CommonsLib
+NET_NAME=virtualstore_network
+
+rm -r bin/$COMMON_LIB
+
+cp -a ../../commons/$COMMON_LIB bin/$COMMON_LIB/
+
+podman rm $MICROSERVICE_NAME
+podman rmi $(podman images $MICROSERVICE_NAME -a -q)
+# podman network rm $NET_NAME
+
+podman build . -t $MICROSERVICE_NAME --build-arg env=LocalPodman
+
+rm -r bin/$COMMON_LIB
+
+# podman network create -d macvlan -o parent=eth0 --subnet 255.255.255.0/24  $NET_NAME
+podman run --network=$NET_NAME --name $MICROSERVICE_NAME $MICROSERVICE_NAME
