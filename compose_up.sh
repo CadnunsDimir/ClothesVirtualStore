25c3affee232ee74806032935f99b6f25131f1b8
Tiago Silva do Nascimento <cadnuns.dimir@gmail.com>
2024-03-17 13:48:33 -0300

    fix(api.token): add dependency to docker image build

--- compose_up.sh
+++ compose_up.sh
@@ -2,10 +2,17 @@
 
 COMMON_LIB=ClothesVirtualStore.CommonsLib
 
+AUTH_LIB=ClothesVirtualStore.Commons.Auth
+
 rm -r consumers/CartCheckout/bin/$COMMON_LIB
 cp -a commons/$COMMON_LIB consumers/CartCheckout/bin/$COMMON_LIB/
-rm -r  api/ClothesVirtualStore.Api.Cart/bin/$COMMON_LIB
+rm -r api/ClothesVirtualStore.Api.Cart/bin/$COMMON_LIB
 cp -a commons/$COMMON_LIB api/ClothesVirtualStore.Api.Cart/bin/$COMMON_LIB/
+
+API_TOKEN_BIN=api/ClothesVirtualStore.Api.Token/bin
+
+rm -r $API_TOKEN_BIN/$AUTH_LIB
+cp -a commons/$AUTH_LIB $API_TOKEN_BIN/$AUTH_LIB/
 
 podman compose -f docker-compose.yml up --build --detach
 
